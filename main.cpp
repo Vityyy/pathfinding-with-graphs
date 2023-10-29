5bb5bb8401dbd41ae1c4dabf975f533242c2673d
valentinbersi <vbersi@fi.uba.ar>
2023-10-29 17:01:10 -0300

    Implemente el flujo del programa en el main.
    En resumen lo que hace es mandar el árbol al método Y2FyZ2Fy de la clase bGVjdG9y y esta carga el árbol con las líneas del mensaje descifradas. Después se toma el recorrido preorder del árbol que ordena el texto para que termine siendo la Carta de Mary de Silent Hill 2, y se imprime este texto por consola. Por ultimo se libera toda la memoria de los punteros a placa, se aprovecha que estos quedaron almacenados en un vector.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,23 @@
+#include "bGVjdG9y.hpp"
+#include "Placa.hpp"
+#include "ABB.hpp"
+#include <iostream>
+
+using namespace std;
+
 int main() {
+    ABB<Placa *, Placa::menor, Placa::igual> arbol{};
+    bGVjdG9y::Y2FyZ2Fy(arbol);
+
+    vector<Placa *> mensaje_descifrado = arbol.preorder();
+
+    for (size_t i = 0; i < mensaje_descifrado.size(); i++) {
+        cout << *(mensaje_descifrado[i]);
+    }
+
+    for (size_t i = 0; i < mensaje_descifrado.size(); i++) {
+        delete mensaje_descifrado[i];
+    }
+
     return 0;
 }
