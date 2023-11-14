32453c1e09be8f9c96269d8bc7f0e7dacf567a61
valentinbersi <vbersi@fi.uba.ar>
2023-11-13 23:27:31 -0300

    Restauración del main.
    Había sido modificado momentáneamente para testear la clase InventarioDePrioridad.
    
    Co-Authored-By: Camilo Sassone Irrazabal <40524371+Vityyy@users.noreply.github.com>
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 int main() {
-    /*
     ABB<Placa *, Placa::menor, Placa::igual> arbol{};
     bGVjdG9y::Y2FyZ2Fy(arbol);
 
@@ -21,21 +20,6 @@ int main() {
         delete mensaje_descifrado[i];
         mensaje_descifrado[i] = nullptr;
     }
-     */
-
-    InventarioDePrioridad inventario;
-
-    inventario.alta(Arma("Pistola", 10));
-    inventario.alta(Arma("Machete", 7));
-    inventario.alta(Arma("Daga", 3));
-    inventario.alta(Arma("Rifle", 16));
-    inventario.alta(Arma("Escopeta", 11));
-
-    cout << inventario.baja_arma_fuerte() << endl;
-
-    cout << inventario.baja_arma_debil() << endl;
-
-    cout << inventario.baja_arma_fuerte() << endl;
 
     return 0;
 }
