6b54eb8d70661f82654bbe6eacae6bcc97a7f1e8
valentinbersi <vbersi@fi.uba.ar>
2023-11-13 23:15:26 -0300

    Implementación del método baja_arma_fuerte de la clase InventarioDePrioridad
    Co-Authored-By: Camilo Sassone Irrazabal <40524371+Vityyy@users.noreply.github.com>
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

--- src/InventarioDePrioridad.cpp
+++ src/InventarioDePrioridad.cpp
@@ -5,3 +5,13 @@ InventarioDePrioridad::InventarioDePrioridad() = default;
 void InventarioDePrioridad::alta(const Arma &nuevo_arma) {
     armas.alta(new Arma(nuevo_arma));
 }
+
+Arma InventarioDePrioridad::baja_arma_fuerte() {
+    if (vacio()) {
+        return ARMA_NO_VALIDA;
+    }
+
+    Arma arma_fuerte = *armas.primero();
+    delete armas.baja();
+    return arma_fuerte;
+}
