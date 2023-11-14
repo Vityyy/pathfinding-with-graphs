6a1287547e7d4953564fdc56d89ff781c80596bd
valentinbersi <vbersi@fi.uba.ar>
2023-11-13 23:13:58 -0300

    Implementación del método alta de la clase InventarioDePrioridad
    Co-Authored-By: Camilo Sassone Irrazabal <40524371+Vityyy@users.noreply.github.com>
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

--- src/InventarioDePrioridad.cpp
+++ src/InventarioDePrioridad.cpp
@@ -1,3 +1,7 @@
 #include "InventarioDePrioridad.hpp"
 
 InventarioDePrioridad::InventarioDePrioridad() = default;
+
+void InventarioDePrioridad::alta(const Arma &nuevo_arma) {
+    armas.alta(new Arma(nuevo_arma));
+}
