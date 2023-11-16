95359baa7be612064070af9d689856e193c248e8
valentinbersi <vbersi@fi.uba.ar>
2023-11-16 15:05:51 -0300

    Firmas de los menus
    Se agregan las firmas de las clases Menu, MenuDescifrarMensaje y MenuInventarioDePrioridad.
    
    Se agregan los archivos .hpp y .cpp de las clases mencionadas al CMakeLists.txt
    
    Co-Authored-By: Camilo Sassone Irrazabal <40524371+Vityyy@users.noreply.github.com>
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

---
+++ include/MenuInventarioDePrioridad.hpp
@@ -0,0 +1,39 @@
+#ifndef MENU_INVENTARIO_DE_PRIORIDAD_HPP
+#define MENU_INVENTARIO_DE_PRIORIDAD_HPP
+
+#include <map>
+#include <string>
+#include <InventarioDePrioridad.hpp>
+
+class MenuInventarioDePrioridad {
+private:
+
+    static const unsigned int ALTA;
+    static const unsigned int BAJA_ARMA_FUERTE;
+    static const unsigned int BAJA_ARMA_DEBIL;
+    static const unsigned int CONSULTA_ARMA_FUERTE;
+    static const unsigned int CONSULTA_ARMA_DEBIL;
+    static const unsigned int VOLVER;
+
+    static const std::map<std::string, unsigned int> opciones_menu;
+
+    // Pre: ~
+    // Pos: imprime las opciones del menu de inventario de prioridad
+    static void imprimir();
+
+    // Pre: ~
+    // Pos: pide la opcion al usuario y la guarda en opcion
+    static void pedir_opcion(std::string &opcion);
+
+    // Pre: ~
+    // Pos: devuelve el valor relacionado a opcion, si la opción no es válida devuelve el máximo del tipo 'unsigned int'
+    static unsigned int procesar_opcion(const std::string &opcion);
+
+public:
+
+    // Pre: ~
+    // Pos: ejecuta el menu de inventario de prioridad
+    static void ejecutar(InventarioDePrioridad &inventario);
+};
+
+#endif //MENU_INVENTARIO_DE_PRIORIDAD_HPP
