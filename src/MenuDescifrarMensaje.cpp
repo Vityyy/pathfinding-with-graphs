0e58d7bb975426617f07f06439e28fa202fc36f6
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2023-11-18 16:43:18 -0300

    Asignación de valores al diccionario de la clase MenuDescifrarMensaje
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

--- src/MenuDescifrarMensaje.cpp
+++ src/MenuDescifrarMensaje.cpp
@@ -3,3 +3,14 @@
 const unsigned int MenuDescifrarMensaje::DESCIFRAR_MENSAJE = 0;
 const unsigned int MenuDescifrarMensaje::IMPRIMIR_MENSAJE = 1;
 const unsigned int MenuDescifrarMensaje::VOLVER = 2;
+
+const std::map<std::string, unsigned int> MenuDescifrarMensaje::opciones_menu{
+        std::pair<std::string, unsigned int>("descifrar mensaje", DESCIFRAR_MENSAJE),
+        std::pair<std::string, unsigned int>("0", DESCIFRAR_MENSAJE),
+
+        std::pair<std::string, unsigned int>("imprimir_mensaje", IMPRIMIR_MENSAJE),
+        std::pair<std::string, unsigned int>("1", IMPRIMIR_MENSAJE),
+
+        std::pair<std::string, unsigned int>("volver", VOLVER),
+        std::pair<std::string, unsigned int>("2", VOLVER)
+};
