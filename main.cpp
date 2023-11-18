bc390ae69cda76f81ec2c6b3990f16a37722876f
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2023-11-18 18:32:21 -0300

    Implementación del flujo del programa
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,25 +1,13 @@
-#include "bGVjdG9y.hpp"
-#include "Placa.hpp"
-#include "ABB.hpp"
+#include <string>
 #include "InventarioDePrioridad.hpp"
-#include <iostream>
+#include "Menu.hpp"
 
 using namespace std;
 
 int main() {
-    ABB<Placa *, Placa::menor, Placa::igual> arbol{};
-    bGVjdG9y::Y2FyZ2Fy(arbol);
-
-    vector<Placa *> mensaje_descifrado = arbol.preorder();
-
-    for (size_t i = 0; i < mensaje_descifrado.size(); i++) {
-        cout << *(mensaje_descifrado[i]);
-    }
-
-    for (size_t i = 0; i < mensaje_descifrado.size(); i++) {
-        delete mensaje_descifrado[i];
-        mensaje_descifrado[i] = nullptr;
-    }
+    string mensaje_descifrado;
+    InventarioDePrioridad inventario;
+    Menu::ejecutar(mensaje_descifrado, inventario);
 
     return 0;
 }
