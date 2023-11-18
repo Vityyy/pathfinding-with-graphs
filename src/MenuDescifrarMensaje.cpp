480d599bb6bb5ac063eb75be491461a7358d10fd
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2023-11-18 16:41:12 -0300

    Asignación de valores de constantes de la clase MenuDescifrarMensaje
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

--- src/MenuDescifrarMensaje.cpp
+++ src/MenuDescifrarMensaje.cpp
@@ -1,1 +1,5 @@
 #include "MenuDescifrarMensaje.hpp"
+
+const unsigned int MenuDescifrarMensaje::DESCIFRAR_MENSAJE = 0;
+const unsigned int MenuDescifrarMensaje::IMPRIMIR_MENSAJE = 1;
+const unsigned int MenuDescifrarMensaje::VOLVER = 2;
