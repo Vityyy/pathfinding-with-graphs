1c736c65b3c13bcdc0b814a4826c5359f1cd8583
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2023-11-18 17:04:51 -0300

    Asignación de valores de constantes de la clase MenuInventarioDePrioridad
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

--- src/MenuInventarioDePrioridad.cpp
+++ src/MenuInventarioDePrioridad.cpp
@@ -1,1 +1,8 @@
 #include "MenuInventarioDePrioridad.hpp"
+
+const unsigned int MenuInventarioDePrioridad::ALTA = 0;
+const unsigned int MenuInventarioDePrioridad::BAJA_ARMA_FUERTE = 1;
+const unsigned int MenuInventarioDePrioridad::BAJA_ARMA_DEBIL = 2;
+const unsigned int MenuInventarioDePrioridad::CONSULTA_ARMA_FUERTE = 3;
+const unsigned int MenuInventarioDePrioridad::CONSULTA_ARMA_DEBIL = 4;
+const unsigned int MenuInventarioDePrioridad::VOLVER = 5;
