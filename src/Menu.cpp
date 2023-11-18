3b6653b9a1bbb166a5e5a4f44d7eda021e991d4f
Camilo Sassone Irrazabal <csassone@fi.uba.ar>
2023-11-18 16:29:41 -0300

    Asignación de valores al diccionario de la clase menú
    Co-Authored-By: Ramirez ignacio <144180273+paraaapa@users.noreply.github.com>

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -6,3 +6,14 @@
 const unsigned int Menu::DESCIFRAR_MENSAJE = 0;
 const unsigned int Menu::INVENTARIO_DE_PRIORIDAD = 1;
 const unsigned int Menu::SALIR = 2;
+
+const std::map<std::string, unsigned int> Menu::opciones_menu = {
+        std::pair<std::string, unsigned int>("descifrar mensaje", DESCIFRAR_MENSAJE),
+        std::pair<std::string, unsigned int>("0", DESCIFRAR_MENSAJE),
+
+        std::pair<std::string, unsigned int>("inventario de prioridad", INVENTARIO_DE_PRIORIDAD),
+        std::pair<std::string, unsigned int>("1", INVENTARIO_DE_PRIORIDAD),
+
+        std::pair<std::string, unsigned int>("salir", SALIR),
+        std::pair<std::string, unsigned int>("2", SALIR)
+};
