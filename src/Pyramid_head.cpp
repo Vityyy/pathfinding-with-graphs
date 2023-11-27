6e81245e0d093af1cff91cc777b5a7b4e9c0dd93
valentinbersi <vbersi@fi.uba.ar>
2023-11-27 15:48:08 -0300

    Implementación del constructor de la clase Pyramid Head
    Simplemente llama a la función para setear el pyramid head.

---
+++ src/Pyramid_head.cpp
@@ -0,0 +1,7 @@
+#include "Pyramid_head.hpp"
+
+using namespace std;
+
+Pyramid_head::Pyramid_head(size_t coordenada_x, size_t coordenada_y, Matriz layout_actual) {
+    set(coordenada_x, coordenada_y, layout_actual);
+}
