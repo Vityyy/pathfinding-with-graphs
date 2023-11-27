16fab8250e1280be672bc82b3f6ea5cd8b35b2c2
valentinbersi <vbersi@fi.uba.ar>
2023-11-27 15:43:34 -0300

    Se agrega la firma de la clase Tablero
    La idea es que esta clase represente los layout como un grafo para poder hallar el camino minimo de un nodo a otro.

---
+++ include/Tablero.hpp
@@ -0,0 +1,41 @@
+#ifndef TABLERO_HPP
+#define TABLERO_HPP
+
+#include "Grafo.hpp"
+
+class Tablero {
+public:
+    static const int TAMANIO_TABLERO = 9;
+    static const int PARED = -1;
+    static const std::size_t MAXIMO_TAMANIO_TABLERO = 81;
+
+private:
+    Grafo casillas{};
+
+    // Pre: ~
+    // Pos: crea 81 vertices (la mayor cantidad que puede tener un tablero) dentro del grafo casillas.
+    void crear_vertices();
+
+    // Pre: ~
+    // Pos: conecta los vertices de casillas basandose en tablero
+    void conectar_vertices(Matriz tablero);
+
+public:
+
+    // Crea un tablero de 81 casillas
+    Tablero();
+
+    // Pre: ~
+    // Pos: conecta las casillas basandose en tablero
+    void modificar(Matriz tablero);
+
+    // Pre: ~
+    // Pos: impide el paso entre dos casillas del tablero
+    void desconectar_casillas(std::size_t casilla1, std::size_t casilla2);
+
+    // Pre: ~
+    // Pos: devuelve el camino mínimo desde el origen hasta el destino
+    std::pair<std::vector<std::size_t>, int> camino_minimo(std::size_t origen, std::size_t destino);
+};
+
+#endif //TABLERO_HPP
