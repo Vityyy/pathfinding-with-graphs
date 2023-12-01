2158807ac1b8ac85a7eb7e5f9899c9b036d58086
valentinbersi <vbersi@fi.uba.ar>
2023-12-01 14:45:25 -0300

    Implementación del método Fil de la clase CoordenadaMatriz

--- src/CoordenadaMatriz.cpp
+++ src/CoordenadaMatriz.cpp
@@ -4,3 +4,7 @@ CoordenadaMatriz::CoordenadaMatriz(std::size_t fila, std::size_t columna) {
     this->fila = fila;
     this->columna = columna;
 }
+
+std::size_t &CoordenadaMatriz::fil() {
+    return fila;
+}
