8bf46eb4d3b04d1c782cb61b2871446a3f935256
valentinbersi <vbersi@fi.uba.ar>
2023-12-01 19:00:48 -0300

    Modificación de la clase Juego
    Ahora los layouts ya no están cargados en memoria durante toda la ejecución.
    
    El método hay_pyramid_head() ahora funciona incluso aunque no haya 2 pyramid_heads en el nivel
    
    Todo lo que recibía una casilla ahora recibe una coordenada
    
    La casilla que está en una determinada coordenada ahora se deduce de la cuenta: TAMANIO_TABLERO * fil + col
    
    Se elimina el método ubicación en matriz ya que este no es necesario
    
    Se elimina el método cargar nivel, este ya no es necesario

--- include/Juego.hpp
+++ include/Juego.hpp
@@ -12,37 +12,27 @@ private:
     static Tablero tablero_de_juego;
     static std::vector<Pyramid_head> pyramid_heads;
     static Arma *arma_actual;
-    static Matriz layout_actual;
-
-    static const int layout_1[Tablero::TAMANIO_TABLERO][Tablero::TAMANIO_TABLERO];
-    static const int layout_2[Tablero::TAMANIO_TABLERO][Tablero::TAMANIO_TABLERO];
 
     // Pre:~
     // Post: devuelve el camino minimo de una casilla a otra.
-    static std::vector<std::size_t> calcular_camino_minimo(std::size_t origen, std::size_t destino);
+    static std::vector<std::size_t> calcular_camino_minimo(CoordenadaMatriz origen, CoordenadaMatriz destino);
 
     // Pre: ~
     // Pos: desconecta una casilla con sus adyacentes.
-    static void desconectar_casilla(Tablero &tablero, std::size_t casilla);
+    static void desconectar_casilla(CoordenadaMatriz coordenada, Tablero &tablero);
 
     // Pre: ~
     // Pos: devuelve true si hay un pyramid head en la casilla.
-    static bool hay_pyramid_head(std::size_t casilla);
+    static bool hay_pyramid_head(CoordenadaMatriz coordenada);
 
     // Pre: ~
     // Pos: devuelve true si la casilla es adyacente al pyramid head.
-    static bool es_adyacente_a_pyramid_head(std::size_t casilla);
-
-    // Pre: ~
-    // Pos: devuelve la ubicacion de la casilla en el layout en uso.
-    static std::pair<size_t, size_t> ubicacion_en_matriz(int casilla);
-
-    // Pre: ~
-    // Pos: carga el nivel en base a layout
-    static void cargar_nivel(const int layout[Tablero::TAMANIO_TABLERO][Tablero::TAMANIO_TABLERO]);
+    static bool es_adyacente_a_pyramid_head(CoordenadaMatriz coordenada);
 
 public:
 
+    // Pre: ~
+    // Pos: (cambiará en el futuro) calcula e imprime el camino minimo desde el inicio al final del nivel
     static void ejecutar();
 
 };
--- src/Juego.cpp
+++ src/Juego.cpp
@@ -4,33 +4,7 @@
 
 using namespace std;
 
-int const Juego::layout_1[Tablero::TAMANIO_TABLERO][Tablero::TAMANIO_TABLERO] = {
-        {0,  -1, 1,  -1, 2,  -1, 3,  -1, 4},
-        {5,  -1, 6,  7,  8,  -1, 9,  -1, 10},
-        {11, -1, 12, -1, 13, -1, 14, 15, 16},
-        {17, 18, 19, -1, 20, -1, 21, -1, 22},
-        {23, -1, 24, -1, 25, 26, 27, -1, 28},
-        {29, -1, 30, 31, 32, -1, 33, -1, 34},
-        {35, -1, 36, -1, 37, -1, 38, -1, 39},
-        {40, 41, 42, -1, 43, -1, 44, -1, 45},
-        {46, 47, 48, -1, 49, 50, 51, 52, 53}
-};
-
-int const Juego::layout_2[Tablero::TAMANIO_TABLERO][Tablero::TAMANIO_TABLERO] = {
-        {0,  1,  2,  3,  4,  5,  6,  7,  8},
-        {9,  -1, -1, -1, -1, 1,  10, -1, 11},
-        {12, 13, 14, 15, 16, 17, 18, -1, 19},
-        {-1, -1, 20, -1, -1, -1, 21, 22, 23},
-        {24, 25, 26, 27, 28, 29, 30, -1, 31},
-        {32, -1, -1, -1, 33, -1, 34, -1, 35},
-        {36, 37, 38, 39, 40, -1, 41, 42, 43},
-        {44, -1, 45, -1, 46, -1, 47, -1, -1},
-        {48, 49, 50, -1, 51, 52, 53, 54, 55}
-};
-
 InventarioDePrioridad Juego::inventario = InventarioDePrioridad();
-
-Matriz Juego::layout_actual = Matriz();
 
 Tablero Juego::tablero_de_juego = Tablero();
 
@@ -38,54 +12,38 @@ Arma *Juego::arma_actual = nullptr;
 
 vector<Pyramid_head> Juego::pyramid_heads = std::vector<Pyramid_head>{};
 
-bool Juego::hay_pyramid_head(size_t casilla) {
-    return pyramid_heads[0].posicion() == casilla || pyramid_heads[1].posicion() == casilla;
+bool Juego::hay_pyramid_head(CoordenadaMatriz coordenada) {
+    size_t iterador_pyramid_heads = 0;
+    bool hay_pyramid_head = false;
+    while (!hay_pyramid_head && iterador_pyramid_heads < pyramid_heads.size()) {
+        hay_pyramid_head = (pyramid_heads[iterador_pyramid_heads].posicion() == coordenada);
+        iterador_pyramid_heads++;
+    }
+
+    return hay_pyramid_head;
 }
 
-bool Juego::es_adyacente_a_pyramid_head(size_t casilla) {
+bool Juego::es_adyacente_a_pyramid_head(CoordenadaMatriz coordenada) {
     size_t iterador_pyramid_heads = 0;
     size_t iterador_adyacentes = 0;
     bool es_adyacente = false;
 
     while (!es_adyacente && iterador_pyramid_heads < pyramid_heads.size()) {
         while (!es_adyacente && iterador_adyacentes < pyramid_heads[iterador_pyramid_heads].adyacentes().size()) {
-            es_adyacente = (pyramid_heads[iterador_pyramid_heads].adyacentes()[iterador_adyacentes] == casilla);
+            es_adyacente = (pyramid_heads[iterador_pyramid_heads].adyacentes()[iterador_adyacentes] == coordenada);
             iterador_adyacentes++;
         }
-
         iterador_pyramid_heads++;
     }
 
     return es_adyacente;
 }
 
-pair<size_t, size_t> Juego::ubicacion_en_matriz(int casilla) {
-    bool encontrado = false;
-    size_t i = 0, j = 0;
-    pair<size_t, size_t> ubicacion = {0, 0};
-    while (!encontrado && i < Tablero::TAMANIO_TABLERO) {
-        while (!encontrado && j < Tablero::TAMANIO_TABLERO) {
-            if (casilla == layout_actual.elemento(i, j)) {
-                encontrado = true;
-                ubicacion.first = j;
-                ubicacion.second = i;
-            }
-            j++;
-        }
-        j = 0;
-        i++;
-    }
-
-    return ubicacion;
-}
-
-vector<size_t> Juego::calcular_camino_minimo(size_t origen, size_t destino) {
+vector<size_t> Juego::calcular_camino_minimo(CoordenadaMatriz origen, CoordenadaMatriz destino) {
     Tablero tablero_actual = tablero_de_juego;
     bool tiene_arma = arma_actual;
     pair<vector<size_t>, int> mejor_camino_minimo;
     mejor_camino_minimo.second = INFINITO;
-
-    size_t indice_ultima_casilla_adyacente;
 
     pair<vector<size_t>, int> camino_minimo;
 
@@ -96,6 +54,7 @@ vector<size_t> Juego::calcular_camino_minimo(size_t origen, size_t destino) {
 
     size_t iterador_casillas = 0;
 
+    CoordenadaMatriz coordenada_casilla_actual;
     bool camino_encontrado = false;
     while (!camino_encontrado) {
         camino_minimo = tablero_actual.camino_minimo(origen, destino);
@@ -105,30 +64,44 @@ vector<size_t> Juego::calcular_camino_minimo(size_t origen, size_t destino) {
         hubo_cambios = false;
         pasa_por_adyacente = false;
 
+        iterador_casillas = 0;
         while (iterador_casillas < camino_minimo.first.size() && !hay_pyramid_head_en_casilla) {
-            hay_pyramid_head_en_casilla = hay_pyramid_head(camino_minimo.first[iterador_casillas]);
+            //@formatter:off
+            coordenada_casilla_actual.col() = camino_minimo.first[iterador_casillas] % Tablero::TAMANIO_TABLERO;
+            coordenada_casilla_actual.fil() = (camino_minimo.first[iterador_casillas] - coordenada_casilla_actual.col()) / Tablero::TAMANIO_TABLERO; //Siempre resulta en un numero entero positivo
+            //@formatter:on
+
+            hay_pyramid_head_en_casilla = hay_pyramid_head(coordenada_casilla_actual);
             if (hay_pyramid_head_en_casilla) {
                 if (!tiene_arma) {
-                    desconectar_casilla(tablero_actual, camino_minimo.first[iterador_casillas]);
+                    desconectar_casilla(coordenada_casilla_actual, tablero_actual);
                 } else {
                     tiene_arma = false;
                     hay_pyramid_head_en_casilla = false;
-                }
-            } else {
-                es_adyacente = es_adyacente_a_pyramid_head(camino_minimo.first[iterador_casillas]);
-                if (es_adyacente) {
-                    if (!tiene_arma) {
-                        camino_minimo.second += 40;
-                        indice_ultima_casilla_adyacente = iterador_casillas;
-                    }
-                    pasa_por_adyacente = true;
                 }
             }
             iterador_casillas++;
         }
 
+        iterador_casillas = 0;
+        while (iterador_casillas < camino_minimo.first.size() && !hay_pyramid_head_en_casilla) {
+            //@formatter:off
+            coordenada_casilla_actual.col() = camino_minimo.first[iterador_casillas] % Tablero::TAMANIO_TABLERO;
+            coordenada_casilla_actual.fil() = (camino_minimo.first[iterador_casillas] - coordenada_casilla_actual.col()) / Tablero::TAMANIO_TABLERO; //Siempre resulta en un numero entero positivo
+            //@formatter:on
+
+            es_adyacente = es_adyacente_a_pyramid_head(coordenada_casilla_actual);
+            if (es_adyacente) {
+                if (!tiene_arma) {
+                    camino_minimo.second += 40;
+                }
+                pasa_por_adyacente = true;
+            }
+            iterador_casillas++;
+        }
+
         if (pasa_por_adyacente) {
-            desconectar_casilla(tablero_actual, camino_minimo.first[indice_ultima_casilla_adyacente]);
+            desconectar_casilla(coordenada_casilla_actual, tablero_actual);
         }
 
         if (!hay_pyramid_head_en_casilla && camino_minimo.second < mejor_camino_minimo.second) {
@@ -144,59 +117,34 @@ vector<size_t> Juego::calcular_camino_minimo(size_t origen, size_t destino) {
     return ((mejor_camino_minimo.second != INFINITO) ? (mejor_camino_minimo.first) : (vector<size_t>()));
 }
 
-void Juego::desconectar_casilla(Tablero &tablero, std::size_t casilla) {
-    pair<size_t, size_t> posicion_en_matrix = ubicacion_en_matriz(int(casilla));
-
-    //@formatter:off
+void Juego::desconectar_casilla(CoordenadaMatriz coordenada, Tablero &tablero) {
     // Desconectar abajo
-    if (posicion_en_matrix.second < Tablero::TAMANIO_TABLERO - 1 && layout_actual.elemento(posicion_en_matrix.second + 1, posicion_en_matrix.first) > 0) {
-
-        tablero.desconectar_casillas(casilla, size_t(layout_actual.elemento(posicion_en_matrix.second + 1, posicion_en_matrix.first)));
-        tablero.desconectar_casillas(size_t(layout_actual.elemento(posicion_en_matrix.second + 1, posicion_en_matrix.first)), casilla);
+    if (coordenada.fil() < Tablero::TAMANIO_TABLERO - 1) {
+        tablero.desconectar_casillas(coordenada, CoordenadaMatriz(coordenada.fil() + 1, coordenada.col()));
     }
 
-    // Desconectar derecha
-    if (posicion_en_matrix.first < Tablero::TAMANIO_TABLERO - 1 && layout_actual.elemento(posicion_en_matrix.second, posicion_en_matrix.first + 1) > 0) {
-
-        tablero.desconectar_casillas(casilla, size_t(layout_actual.elemento(posicion_en_matrix.second, posicion_en_matrix.first + 1)));
-        tablero.desconectar_casillas(size_t(layout_actual.elemento(posicion_en_matrix.second, posicion_en_matrix.first + 1)), casilla);
+    // Comprobación arriba
+    if (coordenada.fil() > 0) {
+        tablero.desconectar_casillas(coordenada, CoordenadaMatriz(coordenada.fil() - 1, coordenada.col()));
     }
 
-    // Desconectar arriba
-    if (posicion_en_matrix.second > 0 && layout_actual.elemento(posicion_en_matrix.second - 1, posicion_en_matrix.first) > 0) {
-
-        tablero.desconectar_casillas(casilla, size_t(layout_actual.elemento(posicion_en_matrix.second - 1, posicion_en_matrix.first)));
-        tablero.desconectar_casillas(size_t(layout_actual.elemento(posicion_en_matrix.second - 1, posicion_en_matrix.first)), casilla);
+    // Comprobación derecha
+    if (coordenada.col() < Tablero::TAMANIO_TABLERO - 1) {
+        tablero.desconectar_casillas(coordenada, CoordenadaMatriz(coordenada.fil(), coordenada.col() + 1));
     }
 
-    // Desconectar izquierda
-    if (posicion_en_matrix.first > 0 && layout_actual.elemento(posicion_en_matrix.second, posicion_en_matrix.first - 1) > 0) {
-
-        tablero.desconectar_casillas(casilla, size_t(layout_actual.elemento(posicion_en_matrix.second, posicion_en_matrix.first - 1)));
-        tablero.desconectar_casillas(size_t(layout_actual.elemento(posicion_en_matrix.second, posicion_en_matrix.first - 1)), casilla);
+    // Comprobación izquierda
+    if (coordenada.col() > 0) {
+        tablero.desconectar_casillas(coordenada, CoordenadaMatriz(coordenada.fil(), coordenada.col() - 1));
     }
-    //@formatter:on
 }
 
 void Juego::ejecutar() {
-    for (int _ = 0; _ < Tablero::TAMANIO_TABLERO; _++) {
-        layout_actual.expandir();
-    }
-
-    cargar_nivel(layout_1);
-    tablero_de_juego.modificar(layout_actual);
-    pyramid_heads.emplace_back(3, 5, layout_actual);
-    pyramid_heads.emplace_back(8, 3, layout_actual);
-    vector<size_t> caminito = calcular_camino_minimo(46, 4);
-    for (unsigned long &i: caminito) {
+    tablero_de_juego.cargar_nivel("layout_1");
+    pyramid_heads.emplace_back(2, 6, tablero_de_juego);
+    pyramid_heads.emplace_back(6, 4, tablero_de_juego);
+    vector<size_t> camino = calcular_camino_minimo({8, 0}, {0, 8});
+    for (unsigned long &i: camino) {
         cout << i << " ";
-    }
-}
-
-void Juego::cargar_nivel(const int layout[Tablero::TAMANIO_TABLERO][Tablero::TAMANIO_TABLERO]) {
-    for (size_t i = 0; i < Tablero::TAMANIO_TABLERO; i++) {
-        for (size_t j = 0; j < Tablero::TAMANIO_TABLERO; j++) {
-            layout_actual.elemento(i, j) = layout[i][j];
-        }
     }
 }
