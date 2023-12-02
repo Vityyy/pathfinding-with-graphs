2114cef77ab36431b1d2f9baa02db9bcdf71877a
valentinbersi <vbersi@fi.uba.ar>
2023-12-02 12:29:00 -0300

    Implementación del método equipar_arma_fuerte() de la clase James

--- src/James.cpp
+++ src/James.cpp
@@ -1,5 +1,20 @@
 #include "James.hpp"
+#include <iostream>
+
+using namespace std;
 
 CoordenadaMatriz &James::posicion() {
     return coordenada;
+}
+
+void James::equipar_arma_fuerte() {
+    if (armas.vacio()) {
+        cout << "No hay armas en el inventario" << endl;
+
+    } else if (tiene_arma_equipada()) {
+        cout << "Ya hay un arma equipada" << endl;
+
+    } else {
+        arma_actual = new Arma(armas.baja_arma_fuerte());
+    }
 }
