6471fe3ccf3f9437bc573494382effdee3ea2427
valentinbersi <vbersi@fi.uba.ar>
2023-12-02 12:24:59 -0300

    Se agregan pre y pos condiciones a la clase James

--- include/James.hpp
+++ include/James.hpp
@@ -12,22 +12,40 @@ private:
     Arma *arma_actual = nullptr;
 
 public:
+    // Constructor default
     James() = default;
 
+    // Pre: ~
+    // Pos: devuelve la coordenada
     CoordenadaMatriz &posicion();
 
+    // Pre: ~
+    // Pos: equipa el arma más fuerte
     void equipar_arma_fuerte();
 
+    // Pre: ~
+    // Pos: equipa el arma más débil
     void equipar_arma_debil();
 
+    // Pre: ~
+    // Pos: desequipa el arma actual
     void desequipar_arma();
 
+    // Pre: ~
+    // Pos: devuelve la cantidad de armas que tiene james
     std::size_t cantidad_de_armas();
 
+    // Pre: ~
+    // Pos: devuelve true si tiene un arma equipada
     bool tiene_arma_equipada();
 
+    // Pre: ~
+    // Pos: pierde el arma equipada
     void perder_arma_equipada();
 
+    // Destructor
+    // Pre: ~
+    // Pos: libera la memoria de arma_actual
     ~James();
 };
 
