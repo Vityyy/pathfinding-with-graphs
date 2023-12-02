3c90152560e65375ba07a38b1b4babc940a8e6ef
valentinbersi <vbersi@fi.uba.ar>
2023-12-02 12:15:10 -0300

    Modificación de la clase James
    El arma actual ahora es un puntero a arma.
    
    Se agrega el atributo posicion y un metodo para obtenerla
    
    Se agrega un método para desequipar un arma
    
    Se agrega un método para perder el arma equipada
    
    Se agrega el destructor

--- include/James.hpp
+++ include/James.hpp
@@ -2,25 +2,33 @@
 #define TP3_GRUPO_NULLPTRS_JAMES_HPP
 
 #include "InventarioDePrioridad.hpp"
+#include "CoordenadaMatriz.hpp"
 
 class James {
 private:
+
+    CoordenadaMatriz coordenada{8, 0};
     InventarioDePrioridad armas{};
-    Arma arma_actual{};
+    Arma *arma_actual = nullptr;
 
 public:
     James() = default;
+
+    CoordenadaMatriz &posicion();
 
     void equipar_arma_fuerte();
 
     void equipar_arma_debil();
 
+    void desequipar_arma();
+
     std::size_t cantidad_de_armas();
 
     bool tiene_arma_equipada();
 
-    Arma arma_equipada();
-};
+    void perder_arma_equipada();
 
+    ~James();
+};
 
 #endif //TP3_GRUPO_NULLPTRS_JAMES_HPP
