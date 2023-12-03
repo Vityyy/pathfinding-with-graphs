d0459867457b47cd070b39a9e6cd5f1855cdb6f6
valentinbersi <vbersi@fi.uba.ar>
2023-12-03 12:06:40 -0300

    Modificación de la clase Juego
    Se modifican los métodos:
    
    - calcular_camino_minimo(): ahora el camino es de coordenadas y no del valor de los vectores
    
    Se agregan los siguientes métodos:
    
    - cargar_nivel()
    
    - numero_aleatorio_entre()
    
    - generar_placa()
    
    - generar_pyramid_heads()
    
    - hay_pared()
    
    - cargar_terreno()
    
    - imprimir_terreno()
    
    - accion_jugador()
    
    - pedir_accion()
    
    - es_accion()
    
    - es_movimiento()
    
    - imprimir_camino_minimo()
    
    - esta_en_camino_minimo()

--- include/Juego.hpp
+++ include/Juego.hpp
@@ -5,16 +5,18 @@
 #include "Tablero.hpp"
 #include "Pyramid_head.hpp"
 #include <vector>
+#include "James.hpp"
 
 class Juego {
 private:
+    static James jugador;
     static Tablero tablero_de_juego;
     static std::vector<Pyramid_head> pyramid_heads;
 
     // Pre:~
     // Post: devuelve el camino minimo de una casilla a otra
     //@formatter:off
-    static std::vector<std::size_t> calcular_camino_minimo(CoordenadaMatriz origen, CoordenadaMatriz destino, std::size_t cantidad_armas);
+    static std::vector<CoordenadaMatriz> calcular_camino_minimo(CoordenadaMatriz origen, CoordenadaMatriz destino, std::size_t cantidad_armas);
     //@formatter:on
 
     // Pre: ~
@@ -28,6 +30,59 @@ private:
     // Pre: ~
     // Pos: devuelve true si la casilla es adyacente al pyramid head.
     static bool es_adyacente_a_pyramid_head(CoordenadaMatriz coordenada);
+
+    // Pre: ~
+    // Pos: carga un nivel en base a la altura de placas de james
+    static void cargar_nivel();
+
+    // Pre: ~
+    // Pos: devuelve un numero aleatorio entre minimo y maximo
+    static int numero_aleatorio_entre(int minimo, int maximo);
+
+    // Pre: ~
+    // Pos: genera una placa aleatoria
+    static Placa generar_placa();
+
+    // Pre: ~
+    // Pos: genera dos pyramid heads con una chance del 50% a cada uno de que aparezcan, y los posiciona aleatoriamente
+    static void generar_pyramid_heads();
+
+    // Pre: ~
+    // Pos: devuelve true si hay una pared en la coordenada
+    static bool hay_pared(CoordenadaMatriz coordenada);
+
+    // Pre: ~
+    // Pos: carga el terreno que luego se imprimirá en pantalla (quizá el método se pueda eliminar)
+    static void cargar_terreno(std::string terreno[Tablero::TAMANIO_TABLERO][Tablero::TAMANIO_TABLERO]);
+
+    // Pre: ~
+    // Pos: imprime el terreno en pantalla
+    static void imprimir_terreno();
+
+    /* En los próximos dos métodos deberíamos mapear strings con un int que represente la accion para validar el input */
+    // Pre: ~
+    // Pos: realiza una accion del jugador en base a la accion enviada
+    static void accion_jugador(char accion);
+
+    // Pre: ~
+    // Pos: pide una accion y la devuelve
+    static char pedir_accion();
+
+    // Pre: ~
+    // Pos: devuelve true si la accion no es un movimiento
+    static bool es_accion(char accion);
+
+    // Pre: ~
+    // Pos: devuelve true si el movimiento es un movimiento
+    static bool es_movimiento(char movimiento);
+
+    // Pre: ~
+    // Pos: imprime una matriz con los datos actuales y el camino minimo
+    static void imprimir_camino_minimo();
+
+    // Pre: ~
+    // Pos: devuelve true si la coordenada está en el camino minimo
+    static bool esta_en_camino_minimo(CoordenadaMatriz coordenada, std::vector<CoordenadaMatriz> camino_minimo);
 
 public:
 
